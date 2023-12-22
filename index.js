2e0a0a36633e8de77983f36433ed1ad268eeace4
Quan Nguyen <vozermuonnam1@gmail.com>
2023-12-22 22:44:11 +0700

    student in json

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 student = {
-    
+    "firstName": "Q",
+    "lastName": "N",
+    "email": "q@gmail.com",
+    "gender": "F",
+    "country": "VN",
+    "favouriteSubjects": [
+        "code",
+        "football"
+    ],
+    "totalSpentInBooks": 0.00
 }
